c3abfa6a9ae05f65f3e920695184dee52a5d35c5
rits-s <79441429+rits-s@users.noreply.github.com>
2021-09-12 14:09:43 +1000

    first commit

---
+++ GoogleLogin/Ritu/Scenarios/Beta Test.feature
@@ -0,0 +1,4 @@
+﻿Feature: Tafe Beta Server is Running
+
+  Scenario: The Tafe Beta Server is running
+    Given: the TAFE homepage is loaded
---
+++ GoogleLogin/Ritu/Script/StatusCheck.js
@@ -0,0 +1,4 @@
+﻿Given("the TAFE homepage is loaded", function (){
+  // Check to see if the TAFE Beta HomePage loaded.
+  aqObject.CheckProperty(Aliases.browser.tafeHomePage, "Visible", cmpEqual, true);
+});
